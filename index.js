790519d01f95e041c369af1fdcdd8f7181b5543a
jasongalas <jasongalas@gmail.com>
2024-04-20 13:58:49 -0700

    Fleshed out the JavaScript, it is successfully writing the SVG graphic)

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
+//global variables calling inquirer, file system, and the code to generate SVG information
 const inquirer = require("inquirer");
+const fs = require('fs');
+const generateSVG = require('./lib/shapes.js');
+
+ //Creating a function to write the SVG file
+function writeToSVG(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        err ? console.error(err) : console.log('SVG created!');
+    })
+    }
+    
+// Create a function to initialize app
+function init() {
+      //the Q&A for building the logo
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "Give three characters for your logo.",
+                name: "text",
+            },
+            {
+                type: "list",
+                message: "What color do you want to use for the text?",
+                name: "textColor",
+                choices: ["black", "white", "red", "blue", "green", "yellow"],
+            },
+            {
+                type: "list",
+                message: "What shape do you want to use?",
+                name: "shape",
+                choices: ["circle", "triangle", "square"],
+            },
+            {
+                type: "list",
+                message: "What color do you want to use for the shape?",
+                name: "shapeColor",
+                choices: ["red", "blue", "green", "yellow", "cyan", "magenta"],
+            },
+        ]).then((inquirerResponses) => {
+            writeToSVG("./examples/logo.svg", generateSVG({...inquirerResponses}))
+        })
+    }
+    
+// Function call to initialize app
+init();
--- test/index.test.js
+++
@@ -1,1 +0,0 @@
-const Shapes = require('../index.js');
